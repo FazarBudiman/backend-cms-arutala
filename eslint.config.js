96a61d17764108747c7a2ef22c8a49e6582b2d11
FazarBudiman <fazarbudiman1907@gmail.com>
2026-01-13 13:35:15 +0700

    add-eslint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettier from 'eslint-config-prettier'
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      // TypeScript rules
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      // JS rules
+      'no-console': 'off',
+      'no-undef': 'off'
+    }
+  },
+  prettier
+]
--- src/db/pool.ts
+++ src/db/pool.ts
@@ -3,4 +3,4 @@ import  {Pool}  from 'pg'
 
 export const pool: Pool = new Pool({
   connectionString: process.env.SUPABASE_DB_URL!
-})
+});
